ececc5d71177ffc845974d15ca47589917fe44c4
Jonathan Kerk <jonathankerk@gmail.com>
2022-11-22 22:24:01 +0800

    feat(infra): generic bucket module

---
+++ modules/bucket/_variables.tf
@@ -0,0 +1,41 @@
+variable "additional_tags" {
+  type        = map(string)
+  description = "Additional tags to include for resources"
+  default     = {}
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Bucket name"
+}
+
+variable "versioning" {
+  type        = string
+  description = "Versioning configuration, defaults to disabled"
+  default     = "Disabled"
+
+  validation {
+    condition     = can(regex("Enabled|Suspended|Disabled", var.versioning))
+    error_message = "Versioning must be one of \"Enabled|Suspended|Disabled\"."
+  }
+}
+
+variable "public_access_config" {
+  type        = map(map(bool))
+  description = "Public access blocks configuration"
+  default = {
+    acl = {
+      block_public_acls       = true
+      block_public_policy     = true
+      ignore_public_acls      = true
+      restrict_public_buckets = true
+    }
+  }
+}
+
+locals {
+  tags = {
+    "ops/managed-by"     = "Terraform",
+    "ops/module-primary" = "bucket"
+  }
+}
---
+++ modules/bucket/bucket.tf
@@ -0,0 +1,52 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = merge(
+    local.tags,
+    var.additional_tags,
+    {
+      "ops/module" = "bucket",
+      "Name"       = var.bucket_name
+    }
+  )
+}
+
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = var.versioning
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse_config" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  for_each                = var.public_access_config
+  block_public_acls       = each.value.block_public_acls
+  block_public_policy     = each.value.block_public_policy
+  ignore_public_acls      = each.value.ignore_public_acls
+  restrict_public_buckets = each.value.restrict_public_buckets
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership_control" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
