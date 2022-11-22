c450000c44691f7ac74f808a4d9fa00aeebef2d8
Jonathan Kerk <jonathankerk@gmail.com>
2022-11-22 22:05:50 +0800

    chore(infra): terraform fmt

--- modules/vpc/nacl.tf
+++ modules/vpc/nacl.tf
@@ -16,24 +16,24 @@ resource "aws_default_network_acl" "nacl" {
 # TODO, decide if NACL should only accept port 80, or just control via SG?
 resource "aws_network_acl_rule" "ip4_in" {
   network_acl_id = aws_default_network_acl.nacl.id
-  rule_number = 100
-  egress = false
-  protocol = -1
-  rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
+  rule_number    = 100
+  egress         = false
+  protocol       = -1
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
 }
 
 resource "aws_network_acl_rule" "ip4_out" {
   network_acl_id = aws_default_network_acl.nacl.id
-  rule_number = 100
-  egress = true
-  protocol = -1
-  rule_action = "allow"
-  cidr_block = "0.0.0.0/0"
+  rule_number    = 100
+  egress         = true
+  protocol       = -1
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
 }
 
 locals {
-  all_subnet_ids  = concat([for subnet in aws_subnet.private_subnets : subnet.id],
-   [for subnet in aws_subnet.public_subnets : subnet.id])
+  all_subnet_ids = concat([for subnet in aws_subnet.private_subnets : subnet.id],
+  [for subnet in aws_subnet.public_subnets : subnet.id])
 }
 
