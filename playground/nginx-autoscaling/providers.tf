ed823973d30ec879dd0a60dff921220a5c00b2f3
Jonathan Kerk <jonathankerk@gmail.com>
2022-11-23 21:12:16 +0800

    feat(infra): nginx service up

---
+++ playground/nginx-autoscaling/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "s3" {
+    bucket = "jonny-multi-purpose-bucket"
+    key = "terraform/playground/nginx-autoscaling/terraform.tfstate"
+    region = "ap-southeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.40.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+
+  default_tags {
+    tags = {
+      "Contact" : "contact@jonathankerk.com"
+    }
+  }
+}
