ef6136f3390b1ced8d466f11619add902b3dca9c
Jonathan Kerk <contact@jonathankerk.com>
2023-09-16 01:09:29 +0800

    fix: removed aws_network_acl_rule for default_network_acl, added inline rules

--- vpc/nacl.tf
+++ vpc/nacl.tf
@@ -3,6 +3,24 @@ resource "aws_default_network_acl" "nacl" {
 
   subnet_ids = local.all_subnet_ids
 
+  ingress {
+    protocol    = -1
+    rule_no     = 100
+    action      = "allow"
+    cidr_blocok = "0.0.0.0/0"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  egress {
+    protocol    = -1
+    rule_no     = 100
+    action      = "allow"
+    cidr_blocok = "0.0.0.0/0"
+    from_port   = 0
+    to_port     = 0
+  }
+
   tags = merge(
     local.tags,
     var.additional_tags,
@@ -13,21 +31,22 @@ resource "aws_default_network_acl" "nacl" {
   )
 }
 
-# TODO, decide if NACL should only accept port 80, or just control via SG?
-resource "aws_network_acl_rule" "ip4_in" {
-  network_acl_id = aws_default_network_acl.nacl.id
-  rule_number    = 100
-  egress         = false
-  protocol       = -1
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-}
+# This is NOT compatible with aws_default_network_acl
+# # TODO, decide if NACL should only accept port 80, or just control via SG?
+# resource "aws_network_acl_rule" "ip4_in" {
+#   network_acl_id = aws_default_network_acl.nacl.id
+#   rule_number    = 100
+#   egress         = false
+#   protocol       = -1
+#   rule_action    = "allow"
+#   cidr_block     = "0.0.0.0/0"
+# }
 
-resource "aws_network_acl_rule" "ip4_out" {
-  network_acl_id = aws_default_network_acl.nacl.id
-  rule_number    = 100
-  egress         = true
-  protocol       = -1
-  rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-}
+# resource "aws_network_acl_rule" "ip4_out" {
+#   network_acl_id = aws_default_network_acl.nacl.id
+#   rule_number    = 100
+#   egress         = true
+#   protocol       = -1
+#   rule_action    = "allow"
+#   cidr_block     = "0.0.0.0/0"
+# }
